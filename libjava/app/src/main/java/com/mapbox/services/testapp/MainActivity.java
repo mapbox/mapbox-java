7f1df477ef23156c66b7bebeaa375c7f2eb9fd5d
Antonio Zugaldia <antonio@mapbox.com>
2016-04-06 16:42:40 -0400

    app module

---
+++ libjava/app/src/main/java/com/mapbox/services/testapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.mapbox.services.testapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
