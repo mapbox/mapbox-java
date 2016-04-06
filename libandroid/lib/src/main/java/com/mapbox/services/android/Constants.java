f46677f54c163a7e973727cec2827764bf0627cc
Antonio Zugaldia <antonio@mapbox.com>
2016-04-06 16:45:27 -0400

    libandroid constants files

---
+++ libandroid/lib/src/main/java/com/mapbox/services/android/Constants.java
@@ -0,0 +1,14 @@
+package com.mapbox.services.android;
+
+/**
+ * Created by antonio on 1/30/16.
+ */
+public class Constants {
+
+    /*
+     * Namespace to avoid constants collision
+     */
+
+    private final static String PACKAGE_NAME = "com.mapbox.services.android";
+
+}
