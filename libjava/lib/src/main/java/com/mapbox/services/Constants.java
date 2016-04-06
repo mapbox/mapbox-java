57ed0777f8b4e1c535e8d0ad35bd51c76009acfd
Antonio Zugaldia <antonio@mapbox.com>
2016-04-06 16:35:25 -0400

    add java constants file

---
+++ libjava/lib/src/main/java/com/mapbox/services/Constants.java
@@ -0,0 +1,28 @@
+package com.mapbox.services;
+
+public class Constants {
+
+    public final static String BASE_API_URL = "https://api.mapbox.com";
+
+    /*
+     * For polyline encoding/decoding
+     */
+
+    public final static int GOOGLE_PRECISION = 5;
+    public final static int OSRM_PRECISION = 6;
+
+    /*
+     * Mapbox default styles
+     * https://www.mapbox.com/developers/api/styles/
+     */
+
+    public final static String MAPBOX_USER = "mapbox";
+
+    public final static String MAPBOX_STYLE_STREETS = "streets-v8";
+    public final static String MAPBOX_STYLE_LIGHT = "light-v8";
+    public final static String MAPBOX_STYLE_DARK = "dark-v8";
+    public final static String MAPBOX_STYLE_EMERALD = "emerald-v8";
+    public final static String MAPBOX_STYLE_SATELLITE = "satellite-v8";
+    public final static String MAPBOX_STYLE_SATELLITE_HYBRID = "satellite-hybrid-v8";
+
+}
