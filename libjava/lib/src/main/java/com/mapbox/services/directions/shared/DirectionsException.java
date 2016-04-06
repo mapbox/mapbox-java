9b73c55d5ed3748b458828204ebf38ea1157c22f
Antonio Zugaldia <antonio@mapbox.com>
2016-04-06 16:36:52 -0400

    add java directions service shared package

---
+++ libjava/lib/src/main/java/com/mapbox/services/directions/shared/DirectionsException.java
@@ -0,0 +1,12 @@
+package com.mapbox.services.directions.shared;
+
+/**
+ * Generic Exception for all things directions
+ */
+public class DirectionsException extends Exception {
+
+    public DirectionsException(String message) {
+        super(message);
+    }
+
+}
