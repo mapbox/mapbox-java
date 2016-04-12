c3442bc7eae7bbdb0a7db59ed6571814a8813b5c
Cameron Mace <cameron@mapbox.com>
2016-04-12 15:44:03 -0400

    Javadoc for directions v5 models (#11)
    * Javadoc for directions v5 models.

--- libjava/lib/src/main/java/com/mapbox/services/directions/v5/models/StepManeuver.java
+++ libjava/lib/src/main/java/com/mapbox/services/directions/v5/models/StepManeuver.java
@@ -6,7 +6,7 @@ import com.mapbox.services.commons.models.Position;
 import java.util.Arrays;
 
 /**
- * Created by antonio on 4/5/16.
+ * Gives maneuver information about one {@link LegStep}.
  */
 public class StepManeuver {
 
@@ -18,38 +18,97 @@ public class StepManeuver {
     private String instruction;
     private int exit;
 
+    /**
+     * @return double array of [longitude, latitude] for the snapped coordinate.
+     */
     public double[] getLocation() {
         return location;
     }
 
+    /**
+     * Number between 0 and 360 indicating the clockwise angle from true north to the direction of
+     * travel right before the maneuver.
+     *
+     * @return double with value from 0 to 360.
+     */
     public double getBearingBefore() {
         return bearingBefore;
     }
 
+    /**
+     * Number between 0 and 360 indicating the clockwise angle from true north to the direction of
+     * travel right after the maneuver.
+     *
+     * @return double with value from 0 to 360.
+     */
     public double getBearingAfter() {
         return bearingAfter;
     }
 
+    /**
+     * This indicates the type of maneuver. It can be any of these listed:
+     * <p/>
+     * <ul>
+     * <li>turn - a basic turn into direction of the modifier</li>
+     * <li>new name - the road name changes (after a mandatory turn)</li>
+     * <li>depart - indicates departure from a leg</li>
+     * <li>arrive - indicates arrival to a destination of a leg</li>
+     * <li>merge - merge onto a street</li>
+     * <li>ramp - take a ramp</li>
+     * <li>fork</li>
+     * <li>end of road - road ends in a T intersection</li>
+     * <li>continue - continue on a street after a turn</li>
+     * <li>roundabout - traverse roundabout, has additional field {@link #getExit()}</li>
+     * </ul>
+     *
+     * @return String with type of maneuver.
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     * This indicates the mode of the maneuver.  If type is of turn, the modifier indicates the
+     * change in direction accomplished through the turn. If the type is of depart/arrive, the
+     * modifier indicates the position of waypoint from the current direction of travel.
+     *
+     * @return String with modifier.
+     */
     public String getModifier() {
         return modifier;
     }
 
+    /**
+     * A human-readable instruction of how to execute the returned maneuver.
+     *
+     * @return String with instruction.
+     */
     public String getInstruction() {
         return instruction;
     }
 
+    /**
+     * Roundabouts will have an additional parameter.
+     *
+     * @return int value
+     */
     public int getExit() {
         return exit;
     }
 
+    /**
+     * Converts double array {@link #getLocation()} to a {@link Position}. You'll typically want to
+     * use this format instead of {@link #getLocation()} as it's easier to work with.
+     *
+     * @return {@link Position}.
+     */
     public Position asPosition() {
         return Position.fromCoordinates(location[0], location[1]);
     }
 
+    /**
+     * @return String with all {@link StepManeuver} information within.
+     */
     @Override
     public String toString() {
         return "StepManeuver{" +
