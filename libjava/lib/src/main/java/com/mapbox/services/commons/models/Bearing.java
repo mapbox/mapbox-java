c2226b9d48e3c1284b8bbd17dcb30a11acecea8c
Cameron Mace <cameron@mapbox.com>
2016-04-13 15:11:19 -0400

    added some doc to bearing and position

--- libjava/lib/src/main/java/com/mapbox/services/commons/models/Bearing.java
+++ libjava/lib/src/main/java/com/mapbox/services/commons/models/Bearing.java
@@ -8,19 +8,26 @@ public class Bearing {
     private int direction;
     private int range;
 
-    /*
-     * Constructor
-     */
 
+    /**
+     * Constructor
+     *
+     * @param direction integer value between 0 and 360 indicating the clockwise angle from true north
+     * @param range     TODO
+     */
     public Bearing(int direction, int range) {
         this.direction = direction;
         this.range = range;
     }
 
-    /*
-     * Getters and setters
-     */
+    // ************************* Getters and Setters *************************
 
+    /**
+     * get the bearing direction integer value between 0 and 360 indicating the clockwise angle from
+     * true north
+     *
+     * @return integer value between 0 and 360.
+     */
     public int getDirection() {
         return direction;
     }
@@ -29,6 +36,11 @@ public class Bearing {
         this.direction = direction;
     }
 
+    /**
+     * TODO
+     *
+     * @return TODO
+     */
     public int getRange() {
         return range;
     }
--- libjava/lib/src/main/java/com/mapbox/services/commons/models/Position.java
+++ libjava/lib/src/main/java/com/mapbox/services/commons/models/Position.java
@@ -9,32 +9,55 @@ public class Position {
     private final double latitude;
     private final double altitude;
 
-    /*
-     * Private constructor
-     */
 
+    /**
+     * Private Constructor
+     *
+     * @param longitude double value with positions longitude.
+     * @param latitude  double value with positions latitude.
+     * @param altitude  double value with positions altitude.
+     */
     private Position(double longitude, double latitude, double altitude) {
         this.longitude = longitude;
         this.latitude = latitude;
         this.altitude = altitude;
     }
 
-    /*
-     * Getters
-     */
+    // ******************************* Getters *******************************
 
+    /**
+     * get the positions longitude.
+     *
+     * @return double value with positions longitude.
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * get the positions latitude.
+     *
+     * @return double value with positions latitude.
+     */
     public double getLatitude() {
         return latitude;
     }
 
+    /**
+     * get the positions altitude.
+     *
+     * @return double value with positions altitude.
+     */
     public double getAltitude() {
         return altitude;
     }
 
+    /**
+     * get the position coordinates in a double array. Altitude will be included within array if it
+     * isn't null.
+     *
+     * @return double array with longitude, latitude, and altitude (if it isn't null).
+     */
     public double[] getCoordinates() {
         if (hasAltitude()) {
             return new double[]{getLongitude(), getLatitude(), getAltitude()};
@@ -43,22 +66,34 @@ public class Position {
         }
     }
 
-    /*
-     * Factories
-     */
+    // ****************************** Factories ******************************
 
+    /**
+     * Build a {@link Position} from a double longitude, latitude and a altitude.
+     *
+     * @param longitude double longitude value.
+     * @param latitude  double latitude value.
+     * @param altitude  double altitude value.
+     */
     public static Position fromCoordinates(double longitude, double latitude, double altitude) {
         return new Position(longitude, latitude, altitude);
     }
 
+    /**
+     * Build a {@link Position} from a double longitude and latitude.
+     *
+     * @param longitude double longitude value.
+     * @param latitude  double latitude value.
+     */
     public static Position fromCoordinates(double longitude, double latitude) {
         return new Position(longitude, latitude, Double.NaN);
     }
 
-    /*
-     * Altitude related
+    /**
+     * Check if a position has altitude value.
+     *
+     * @return true if position contains altitude data.
      */
-
     public boolean hasAltitude() {
         return !Double.isNaN(altitude);
     }
