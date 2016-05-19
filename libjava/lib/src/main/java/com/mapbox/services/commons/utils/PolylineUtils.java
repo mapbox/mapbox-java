fcb2b5b839abea8b8496ab65a059cea99535088d
Antonio Zugaldia <antonio@mapbox.com>
2016-05-19 13:42:58 -0700

    port complete

--- libjava/lib/src/main/java/com/mapbox/services/commons/utils/PolylineUtils.java
+++ libjava/lib/src/main/java/com/mapbox/services/commons/utils/PolylineUtils.java
@@ -6,11 +6,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Encoded Polyline Algorithm Format. Some parts from:
- * https://github.com/mapbox/polyline/blob/master/src/polyline.js
- * https://github.com/googlemaps/android-maps-utils/blob/master/library/src/com/google/maps/android/PolyUtil.java
+ * Polyline utils
  */
 public class PolylineUtils {
+
+    /*
+     * Encoded Polyline Algorithm Format. Some parts from:
+     * https://github.com/mapbox/polyline/blob/master/src/polyline.js
+     * https://github.com/googlemaps/android-maps-utils/blob/master/library/src/com/google/maps/android/PolyUtil.java
+     */
 
     /**
      * Decodes an encoded path string into a sequence of Positions.
@@ -91,4 +95,131 @@ public class PolylineUtils {
         result.append(Character.toChars((int) (v + 63)));
     }
 
+    /*
+     * Polyline simplification method. It's a direct port of simplify.js to Java.
+     * See: https://github.com/mourner/simplify-js/blob/master/simplify.js
+     */
+
+    /**
+     * Square distance between 2 points
+     */
+    private static double getSqDist(Position p1, Position p2) {
+        double dx = p1.getLongitude() - p2.getLongitude();
+        double dy = p1.getLatitude() - p2.getLatitude();
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * Square distance from a point to a segment
+     */
+    private static double getSqSegDist(Position p, Position p1, Position p2) {
+        double x = p1.getLongitude();
+        double y = p1.getLatitude();
+        double dx = p2.getLongitude() - x;
+        double dy = p2.getLatitude() - y;
+
+        if (dx != 0 || dy != 0) {
+            double t = ((p.getLongitude() - x) * dx + (p.getLatitude() - y) * dy) / (dx * dx + dy * dy);
+            if (t > 1) {
+                x = p2.getLongitude();
+                y = p2.getLatitude();
+
+            } else if (t > 0) {
+                x += dx * t;
+                y += dy * t;
+            }
+        }
+
+        dx = p.getLongitude() - x;
+        dy = p.getLatitude() - y;
+
+        return dx * dx + dy * dy;
+    }
+
+    /**
+     * Basic distance-based simplification
+     */
+    private static Position[] simplifyRadialDist(Position[] points, double sqTolerance) {
+        Position prevPoint = points[0];
+        ArrayList<Position> newPoints = new ArrayList<>();
+        newPoints.add(prevPoint);
+        Position point = null;
+
+        for (int i = 1, len = points.length; i < len; i++) {
+            point = points[i];
+
+            if (getSqDist(point, prevPoint) > sqTolerance) {
+                newPoints.add(point);
+                prevPoint = point;
+            }
+        }
+
+        if (prevPoint != point) newPoints.add(point);
+
+        return newPoints.toArray(new Position[newPoints.size()]);
+    }
+
+    private static List<Position> simplifyDPStep(Position[] points, int first, int last, double sqTolerance, List<Position> simplified) {
+        double maxSqDist = sqTolerance;
+        int index = 0;
+
+        ArrayList<Position> stepList = new ArrayList<>();
+
+        for (int i = first + 1; i < last; i++) {
+            double sqDist = getSqSegDist(points[i], points[first], points[last]);
+            if (sqDist > maxSqDist) {
+                index = i;
+                maxSqDist = sqDist;
+            }
+        }
+
+        if (maxSqDist > sqTolerance) {
+            if (index - first > 1) {
+                stepList.addAll(simplifyDPStep(points, first, index, sqTolerance, simplified));
+            }
+
+            stepList.add(points[index]);
+
+            if (last - index > 1) {
+                stepList.addAll(simplifyDPStep(points, index, last, sqTolerance, simplified));
+            }
+        }
+
+        return stepList;
+    }
+
+    /**
+     * Simplification using Ramer-Douglas-Peucker algorithm
+     */
+    private static Position[] simplifyDouglasPeucker(Position[] points, double sqTolerance) {
+        int last = points.length - 1;
+
+        ArrayList<Position> simplified = new ArrayList<>();
+        simplified.add(points[0]);
+        simplified.addAll(simplifyDPStep(points, 0, last, sqTolerance, simplified));
+        simplified.add(points[last]);
+        return simplified.toArray(new Position[simplified.size()]);
+    }
+
+    /**
+     * Both algorithms combined for awesome performance
+     */
+    public static Position[] simplify(Position[] points) {
+        return simplify(points, 1.0);
+    }
+
+    public static Position[] simplify(Position[] points, double tolerance) {
+        return simplify(points, tolerance, false);
+    }
+
+    public static Position[] simplify(Position[] points, double tolerance, boolean highestQuality) {
+        if (points.length <= 2) return points;
+
+        double sqTolerance = tolerance * tolerance;
+
+        points = highestQuality ? points : simplifyRadialDist(points, sqTolerance);
+        points = simplifyDouglasPeucker(points, sqTolerance);
+
+        return points;
+    }
 }
--- libjava/lib/src/test/java/com/mapbox/services/utils/PolylineUtilsTest.java
+++ libjava/lib/src/test/java/com/mapbox/services/utils/PolylineUtilsTest.java
@@ -3,6 +3,7 @@ package com.mapbox.services.utils;
 import com.mapbox.services.Constants;
 import com.mapbox.services.commons.geojson.LineString;
 import com.mapbox.services.commons.models.Position;
+import com.mapbox.services.commons.utils.PolylineUtils;
 
 import org.junit.Test;
 
@@ -16,17 +17,57 @@ import static org.junit.Assert.*;
  */
 public class PolylineUtilsTest {
 
+    private final static double DELTA = 1e-15;
+
+    private Position[] points = {
+            Position.fromCoordinates(224.55, 250.15), Position.fromCoordinates(226.91, 244.19), Position.fromCoordinates(233.31, 241.45), Position.fromCoordinates(234.98, 236.06),
+            Position.fromCoordinates(244.21, 232.76), Position.fromCoordinates(262.59, 215.31), Position.fromCoordinates(267.76, 213.81), Position.fromCoordinates(273.57, 201.84),
+            Position.fromCoordinates(273.12, 192.16), Position.fromCoordinates(277.62, 189.03), Position.fromCoordinates(280.36, 181.41), Position.fromCoordinates(286.51, 177.74),
+            Position.fromCoordinates(292.41, 159.37), Position.fromCoordinates(296.91, 155.64), Position.fromCoordinates(314.95, 151.37), Position.fromCoordinates(319.75, 145.16),
+            Position.fromCoordinates(330.33, 137.57), Position.fromCoordinates(341.48, 139.96), Position.fromCoordinates(369.98, 137.89), Position.fromCoordinates(387.39, 142.51),
+            Position.fromCoordinates(391.28, 139.39), Position.fromCoordinates(409.52, 141.14), Position.fromCoordinates(414.82, 139.75), Position.fromCoordinates(427.72, 127.30),
+            Position.fromCoordinates(439.60, 119.74), Position.fromCoordinates(474.93, 107.87), Position.fromCoordinates(486.51, 106.75), Position.fromCoordinates(489.20, 109.45),
+            Position.fromCoordinates(493.79, 108.63), Position.fromCoordinates(504.74, 119.66), Position.fromCoordinates(512.96, 122.35), Position.fromCoordinates(518.63, 120.89),
+            Position.fromCoordinates(524.09, 126.88), Position.fromCoordinates(529.57, 127.86), Position.fromCoordinates(534.21, 140.93), Position.fromCoordinates(539.27, 147.24),
+            Position.fromCoordinates(567.69, 148.91), Position.fromCoordinates(575.25, 157.26), Position.fromCoordinates(580.62, 158.15), Position.fromCoordinates(601.53, 156.85),
+            Position.fromCoordinates(617.74, 159.86), Position.fromCoordinates(622.00, 167.04), Position.fromCoordinates(629.55, 194.60), Position.fromCoordinates(638.90, 195.61),
+            Position.fromCoordinates(641.26, 200.81), Position.fromCoordinates(651.77, 204.56), Position.fromCoordinates(671.55, 222.55), Position.fromCoordinates(683.68, 217.45),
+            Position.fromCoordinates(695.25, 219.15), Position.fromCoordinates(700.64, 217.98), Position.fromCoordinates(703.12, 214.36), Position.fromCoordinates(712.26, 215.87),
+            Position.fromCoordinates(721.49, 212.81), Position.fromCoordinates(727.81, 213.36), Position.fromCoordinates(729.98, 208.73), Position.fromCoordinates(735.32, 208.20),
+            Position.fromCoordinates(739.94, 204.77), Position.fromCoordinates(769.98, 208.42), Position.fromCoordinates(779.60, 216.87), Position.fromCoordinates(784.20, 218.16),
+            Position.fromCoordinates(800.24, 214.62), Position.fromCoordinates(810.53, 219.73), Position.fromCoordinates(817.19, 226.82), Position.fromCoordinates(820.77, 236.17),
+            Position.fromCoordinates(827.23, 236.16), Position.fromCoordinates(829.89, 239.89), Position.fromCoordinates(851.00, 248.94), Position.fromCoordinates(859.88, 255.49),
+            Position.fromCoordinates(865.21, 268.53), Position.fromCoordinates(857.95, 280.30), Position.fromCoordinates(865.48, 291.45), Position.fromCoordinates(866.81, 298.66),
+            Position.fromCoordinates(864.68, 302.71), Position.fromCoordinates(867.79, 306.17), Position.fromCoordinates(859.87, 311.37), Position.fromCoordinates(860.08, 314.35),
+            Position.fromCoordinates(858.29, 314.94), Position.fromCoordinates(858.10, 327.60), Position.fromCoordinates(854.54, 335.40), Position.fromCoordinates(860.92, 343.00),
+            Position.fromCoordinates(856.43, 350.15), Position.fromCoordinates(851.42, 352.96), Position.fromCoordinates(849.84, 359.59), Position.fromCoordinates(854.56, 365.53),
+            Position.fromCoordinates(849.74, 370.38), Position.fromCoordinates(844.09, 371.89), Position.fromCoordinates(844.75, 380.44), Position.fromCoordinates(841.52, 383.67),
+            Position.fromCoordinates(839.57, 390.40), Position.fromCoordinates(845.59, 399.05), Position.fromCoordinates(848.40, 407.55), Position.fromCoordinates(843.71, 411.30),
+            Position.fromCoordinates(844.09, 419.88), Position.fromCoordinates(839.51, 432.76), Position.fromCoordinates(841.33, 441.04), Position.fromCoordinates(847.62, 449.22),
+            Position.fromCoordinates(847.16, 458.44), Position.fromCoordinates(851.38, 462.79), Position.fromCoordinates(853.97, 471.15), Position.fromCoordinates(866.36, 480.77)
+    };
+
+    private Position[] simplified = {
+            Position.fromCoordinates(224.55, 250.15), Position.fromCoordinates(267.76, 213.81), Position.fromCoordinates(296.91, 155.64), Position.fromCoordinates(330.33, 137.57),
+            Position.fromCoordinates(409.52, 141.14), Position.fromCoordinates(439.60, 119.74), Position.fromCoordinates(486.51, 106.75), Position.fromCoordinates(529.57, 127.86),
+            Position.fromCoordinates(539.27, 147.24), Position.fromCoordinates(617.74, 159.86), Position.fromCoordinates(629.55, 194.60), Position.fromCoordinates(671.55, 222.55),
+            Position.fromCoordinates(727.81, 213.36), Position.fromCoordinates(739.94, 204.77), Position.fromCoordinates(769.98, 208.42), Position.fromCoordinates(779.60, 216.87),
+            Position.fromCoordinates(800.24, 214.62), Position.fromCoordinates(820.77, 236.17), Position.fromCoordinates(859.88, 255.49), Position.fromCoordinates(865.21, 268.53),
+            Position.fromCoordinates(857.95, 280.30), Position.fromCoordinates(867.79, 306.17), Position.fromCoordinates(859.87, 311.37), Position.fromCoordinates(854.54, 335.40),
+            Position.fromCoordinates(860.92, 343.00), Position.fromCoordinates(849.84, 359.59), Position.fromCoordinates(854.56, 365.53), Position.fromCoordinates(844.09, 371.89),
+            Position.fromCoordinates(839.57, 390.40), Position.fromCoordinates(848.40, 407.55), Position.fromCoordinates(839.51, 432.76), Position.fromCoordinates(853.97, 471.15),
+            Position.fromCoordinates(866.36, 480.77)
+    };
+
     /*
      * Sample values from
      * https://developers.google.com/maps/documentation/utilities/polylinealgorithm
      */
 
-    private final static double DELTA = 1e-15;
-
     @Test
     public void testDecode() {
         // _p~iF~ps|U_ulLnnqC_mqNvxq`@
-        List<Position> path = com.mapbox.services.commons.utils.PolylineUtils.decode(
+        List<Position> path = PolylineUtils.decode(
                 "_p~iF~ps|U_ulLnnqC_mqNvxq`@", Constants.GOOGLE_PRECISION);
 
         // (38.5, -120.2), (40.7, -120.95), (43.252, -126.453)
@@ -47,7 +88,7 @@ public class PolylineUtilsTest {
         path.add(Position.fromCoordinates(-126.453, 43.252));
 
         // _p~iF~ps|U_ulLnnqC_mqNvxq`@
-        assertEquals(com.mapbox.services.commons.utils.PolylineUtils.encode(
+        assertEquals(PolylineUtils.encode(
                 path, Constants.GOOGLE_PRECISION), "_p~iF~ps|U_ulLnnqC_mqNvxq`@");
     }
 
@@ -79,6 +120,29 @@ public class PolylineUtilsTest {
         assertEquals(path.getCoordinates().get(middle).getLatitude(), 38.901172, DELTA);
         assertEquals(path.getCoordinates().get(last).getLongitude(), -77.035683, DELTA);
         assertEquals(path.getCoordinates().get(last).getLatitude(), 38.898553, DELTA);
+    }
+
+    @Test
+    public void testSimplify() {
+        // Simplifies points correctly with the given tolerance
+        Position[] result = PolylineUtils.simplify(points, 5);
+        assertEquals(simplified.length, result.length);
+        for (int i = 0; i < simplified.length; i++) {
+            assertEquals(simplified[i].getLongitude(), result[i].getLongitude(), DELTA);
+            assertEquals(simplified[i].getLatitude(), result[i].getLatitude(), DELTA);
+        }
+
+        // Just return the points if it has only one point
+        Position[] onePoint = {Position.fromCoordinates(1, 2)};
+        result = PolylineUtils.simplify(onePoint);
+        assertEquals(result.length, 1);
+        assertEquals(result[0].getLongitude(), 1, DELTA);
+        assertEquals(result[0].getLatitude(), 2, DELTA);
+
+        // Just return the points if it has no points
+        Position[] emptyList = {};
+        result = PolylineUtils.simplify(emptyList);
+        assertEquals(result.length, 0);
     }
 
 }
