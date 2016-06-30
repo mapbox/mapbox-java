2618fb9462bee2ff8167ab4f0c10c0fd60b28879
Cameron Mace <cameron@mapbox.com>
2016-06-30 10:16:39 -0400

    Cleaned up the test app (#84)
    * updated SDK latest and removed list padding
    
    * uses Mapbox colors now
    
    * removed unused mainActivity options menu
    
    * update to 4.1.0 final

--- libandroid/app/src/main/java/com/mapbox/services/android/testapp/MainActivity.java
+++ libandroid/app/src/main/java/com/mapbox/services/android/testapp/MainActivity.java
@@ -93,31 +93,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * Menu
-     */
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            Toast.makeText(this, "These are not the settings you are looking for.",
-                    Toast.LENGTH_LONG).show();
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    /*
      * Recycler view
      */
 
