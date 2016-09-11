f1db30d1dea9f9e6ebe9975ec3951c883e738ddd
pgs <pgs@tid.es>
2016-09-11 11:46:17 +0200

    fix Polygon tests for raw array construction

--- libjava/lib/src/test/java/com/mapbox/services/geojson/PolygonTest.java
+++ libjava/lib/src/test/java/com/mapbox/services/geojson/PolygonTest.java
@@ -2,10 +2,8 @@ package com.mapbox.services.geojson;
 
 import com.mapbox.services.commons.geojson.Polygon;
 import com.mapbox.services.commons.models.Position;
-
 import org.junit.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -13,57 +11,89 @@ import static org.junit.Assert.assertFalse;
 
 public class PolygonTest extends BaseGeoJSON {
 
-  @Test
-  public void fromJson() {
-    Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON);
-    assertEquals(geo.getType(), "Polygon");
-    assertEquals(geo.getCoordinates().get(0).get(0).getLongitude(), 100.0, DELTA);
-    assertEquals(geo.getCoordinates().get(0).get(0).getLatitude(), 0.0, DELTA);
-    assertFalse(geo.getCoordinates().get(0).get(0).hasAltitude());
-  }
+    @Test
+    public void fromJson() {
+        Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON);
+        assertEquals(geo.getType(), "Polygon");
+        assertEquals(geo.getCoordinates().get(0).get(0).getLongitude(), 100.0, DELTA);
+        assertEquals(geo.getCoordinates().get(0).get(0).getLatitude(), 0.0, DELTA);
+        assertFalse(geo.getCoordinates().get(0).get(0).hasAltitude());
+    }
 
-  @Test
-  public void fromJsonHoles() {
-    Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES);
-    assertEquals(geo.getType(), "Polygon");
-    assertEquals(geo.getCoordinates().get(0).get(0).getLongitude(), 100.0, DELTA);
-    assertEquals(geo.getCoordinates().get(0).get(0).getLatitude(), 0.0, DELTA);
-    assertFalse(geo.getCoordinates().get(0).get(0).hasAltitude());
-  }
+    @Test
+    public void fromJsonHoles() {
+        Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES);
+        assertEquals(geo.getType(), "Polygon");
+        assertEquals(geo.getCoordinates().get(0).get(0).getLongitude(), 100.0, DELTA);
+        assertEquals(geo.getCoordinates().get(0).get(0).getLatitude(), 0.0, DELTA);
+        assertFalse(geo.getCoordinates().get(0).get(0).hasAltitude());
+    }
 
-  @Test
-  public void toJson() {
-    Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON);
-    compareJson(BaseGeoJSON.SAMPLE_POLYGON, geo.toJson());
-  }
+    @Test
+    public void toJson() {
+        Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON);
+        compareJson(BaseGeoJSON.SAMPLE_POLYGON, geo.toJson());
+    }
 
-  @Test
-  public void toJsonHoles() {
-    Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES);
-    compareJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES, geo.toJson());
-  }
+    @Test
+    public void toJsonHoles() {
+        Polygon geo = Polygon.fromJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES);
+        compareJson(BaseGeoJSON.SAMPLE_POLYGON_HOLES, geo.toJson());
+    }
 
-  @Test
-  public void testFromCoordinates() {
-    // fromCoordinates (list)
-    ArrayList<Position> pointList = new ArrayList<>();
-    pointList.add(Position.fromCoordinates(0, 0));
-    pointList.add(Position.fromCoordinates(0, 100));
-    pointList.add(Position.fromCoordinates(100, 100));
-    pointList.add(Position.fromCoordinates(100, 0));
-    pointList.add(Position.fromCoordinates(0, 0));
-    ArrayList<List<Position>> coordinates = new ArrayList<>();
-    coordinates.add(pointList);
-    Polygon polyList = Polygon.fromCoordinates(coordinates);
+    @Test
+    public void checksEqualityFromCoordinates() {
+        Polygon aPolygon = Polygon.fromCoordinates(new double[][][]{
+                {{100.0, 0.0}, {101.0, 0.0}, {101.0, 1.0}, {100.0, 1.0}, {100.0, 0.0}},
+                {{100.2, 0.2}, {100.8, 0.2}, {100.8, 0.8}, {100.2, 0.8}, {100.2, 0.2}}
+        });
 
-    // fromCoordinates (array)
-    Polygon polyArray = Polygon.fromCoordinates(new double[][][] {{
-      {0, 0}, {0, 100}, {100, 100}, {100, 0}, {0, 0}
-    }});
+        String polygonCoordinates = obtainLiteralCoordinatesFrom(aPolygon);
 
-    // Test equality
-    assertEquals(polyList.getCoordinates(), polyArray.getCoordinates());
-    compareJson(polyList.toJson(), polyArray.toJson());
-  }
+        assertEquals("Polygon: \n" +
+                "Lines: \n" +
+                "Position [longitude=100.0, latitude=0.0, altitude=NaN]\n" +
+                "Position [longitude=101.0, latitude=0.0, altitude=NaN]\n" +
+                "Position [longitude=101.0, latitude=1.0, altitude=NaN]\n" +
+                "Position [longitude=100.0, latitude=1.0, altitude=NaN]\n" +
+                "Position [longitude=100.0, latitude=0.0, altitude=NaN]\n" +
+                "Lines: \n" +
+                "Position [longitude=100.2, latitude=0.2, altitude=NaN]\n" +
+                "Position [longitude=100.8, latitude=0.2, altitude=NaN]\n" +
+                "Position [longitude=100.8, latitude=0.8, altitude=NaN]\n" +
+                "Position [longitude=100.2, latitude=0.8, altitude=NaN]\n" +
+                "Position [longitude=100.2, latitude=0.2, altitude=NaN]\n", polygonCoordinates);
+    }
+
+    @Test
+    public void checksJsonEqualityFromCoordinates() {
+        Polygon aPolygon = Polygon.fromCoordinates(new double[][][]{
+                {{100.0, 0.0}, {101.0, 0.0}, {101.0, 1.0}, {100.0, 1.0}, {100.0, 0.0}},
+                {{100.2, 0.2}, {100.8, 0.2}, {100.8, 0.8}, {100.2, 0.8}, {100.2, 0.2}}
+        });
+
+        String polygonJsonCoordinates = aPolygon.toJson();
+
+        compareJson("{ \"type\": \"Polygon\",\n" +
+                "\"coordinates\": [\n" +
+                "[ [100.0, 0.0], [101.0, 0.0], [101.0, 1.0], [100.0, 1.0], [100.0, 0.0] ],\n" +
+                "[ [100.2, 0.2], [100.8, 0.2], [100.8, 0.8], [100.2, 0.8], [100.2, 0.2] ]\n" +
+                "]\n" +
+                "}", polygonJsonCoordinates);
+    }
+
+    private String obtainLiteralCoordinatesFrom(Polygon polygon) {
+        List<List<Position>> polygonCoordinates = polygon.getCoordinates();
+        StringBuilder literalCoordinates = new StringBuilder();
+        literalCoordinates.append("Polygon: \n");
+        for (List<Position> lines : polygonCoordinates) {
+            literalCoordinates.append("Lines: \n");
+            for (Position point : lines) {
+                literalCoordinates.append(point.toString());
+                literalCoordinates.append("\n");
+            }
+        }
+        return literalCoordinates.toString();
+    }
 
 }
