af4ab12ebfe0d8be0aa7a943a2dc376a905515cb
Antonio Zugaldia <antonio@mapbox.com>
2017-01-05 10:01:55 -0500

    provide warning if token isn't found

--- mapbox/app/src/main/java/com/mapbox/services/android/testapp/MasApplication.java
+++ mapbox/app/src/main/java/com/mapbox/services/android/testapp/MasApplication.java
@@ -1,20 +1,27 @@
 package com.mapbox.services.android.testapp;
 
 import android.app.Application;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.mapbox.mapboxsdk.MapboxAccountManager;
 import com.squareup.leakcanary.LeakCanary;
 
 public class MasApplication extends Application {
 
+  private static final String LOG_TAG = MasApplication.class.getSimpleName();
+
   @Override
   public void onCreate() {
     super.onCreate();
-    if (LeakCanary.isInAnalyzerProcess(this)) {
-      return;
-    }
-    LeakCanary.install(this);
-    MapboxAccountManager.start(getApplicationContext(), Utils.getMapboxAccessToken(getApplicationContext()));
 
+    // Leak canary
+    if (LeakCanary.isInAnalyzerProcess(this)) return;
+    LeakCanary.install(this);
+
+    // Access token
+    String mapboxAccessToken = Utils.getMapboxAccessToken(getApplicationContext());
+    if (TextUtils.isEmpty(mapboxAccessToken)) Log.w(LOG_TAG, "Warning: access token isn't set.");
+    MapboxAccountManager.start(getApplicationContext(), mapboxAccessToken);
   }
 }
