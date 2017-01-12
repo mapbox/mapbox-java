42ec10a890f0d80d26c081e8c9521db92db531f1
Antonio Zugaldia <antonio@mapbox.com>
2017-01-12 14:05:21 -0500

    fix javadoc

--- mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/location/LocationEngineListener.java
+++ mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/location/LocationEngineListener.java
@@ -3,7 +3,7 @@ package com.mapbox.services.android.telemetry.location;
 import android.location.Location;
 
 /**
- * Created by antonio on 1/11/17.
+ * Callback used in LocationEngine
  */
 
 public interface LocationEngineListener {
--- mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/permissions/PermissionsListener.java
+++ mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/permissions/PermissionsListener.java
@@ -3,7 +3,7 @@ package com.mapbox.services.android.telemetry.permissions;
 import java.util.List;
 
 /**
- * Created by antonio on 1/11/17.
+ * Callback used in PermissionsManager
  */
 
 public interface PermissionsListener {
