0a42d232d3f531a2a2f1bdc67f99a4de35089cfd
Antonio Zugaldia <antonio@mapbox.com>
2017-01-12 11:25:41 -0500

    sample location engines implementations

---
+++ mapbox/app/src/main/java/com/mapbox/services/android/testapp/location/GoogleLocationEngine.java
@@ -0,0 +1,158 @@
+package com.mapbox.services.android.testapp.location;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
+import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Sample LocationEngine using Google Play Services
+ */
+public class GoogleLocationEngine implements LocationEngine,
+  GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+  private static final String LOG_TAG = GoogleLocationEngine.class.getSimpleName();
+
+  private static LocationEngine instance;
+
+  private Context context;
+  private CopyOnWriteArrayList<LocationEngineListener> locationListeners;
+  private GoogleApiClient googleApiClient;
+
+  private int priority;
+
+  public GoogleLocationEngine(Context context) {
+    this.context = context;
+    locationListeners = new CopyOnWriteArrayList<>();
+    googleApiClient = new GoogleApiClient.Builder(context)
+      .addConnectionCallbacks(this)
+      .addOnConnectionFailedListener(this)
+      .addApi(LocationServices.API)
+      .build();
+  }
+
+  public static LocationEngine getLocationEngine(Context context) {
+    if (instance == null) {
+      instance = new GoogleLocationEngine(context.getApplicationContext());
+    }
+
+    return instance;
+  }
+
+  @Override
+  public void activate() {
+    if (googleApiClient != null && !googleApiClient.isConnected()) {
+      googleApiClient.connect();
+    }
+  }
+
+  @Override
+  public void deactivate() {
+    if (googleApiClient != null && googleApiClient.isConnected()) {
+      googleApiClient.disconnect();
+    }
+  }
+
+  @Override
+  public boolean isConnected() {
+    return googleApiClient.isConnected();
+  }
+
+  @Override
+  public void onConnected(@Nullable Bundle bundle) {
+    for (LocationEngineListener listener : this.locationListeners) {
+      listener.onConnected();
+    }
+  }
+
+  @Override
+  public void onConnectionSuspended(int cause) {
+    Log.d(LOG_TAG, "Connection suspended: " + cause);
+  }
+
+  @Override
+  public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    Log.d(LOG_TAG, "Connection failed:" + connectionResult.getErrorMessage());
+  }
+
+  @Override
+  public int getPriority() {
+    return priority;
+  }
+
+  @Override
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
+
+  @Override
+  public Location getLastLocation() {
+    if (googleApiClient.isConnected()
+      && PermissionsManager.isPermissionGranted(context, PermissionsManager.FINE_LOCATION_PERMISSION)) {
+      //noinspection MissingPermission
+      return LocationServices.FusedLocationApi.getLastLocation(googleApiClient);
+    }
+
+    return null;
+  }
+
+  @Override
+  public void addLocationEngineListener(LocationEngineListener listener) {
+    if (!this.locationListeners.contains(listener)) {
+      this.locationListeners.add(listener);
+    }
+  }
+
+  @Override
+  public boolean removeLocationEngineListener(LocationEngineListener listener) {
+    return this.locationListeners.remove(listener);
+  }
+
+  @Override
+  public void requestLocationUpdates() {
+    // Common params
+    LocationRequest request = LocationRequest.create()
+      .setFastestInterval(1000)
+      .setSmallestDisplacement(3.0f);
+
+    // Priority matching is straightforward
+    if (priority == LocationEnginePriority.PRIORITY_NO_POWER) {
+      request.setPriority(LocationRequest.PRIORITY_NO_POWER);
+    } else if (priority == LocationEnginePriority.PRIORITY_LOW_POWER) {
+      request.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+    } else if (priority == LocationEnginePriority.PRIORITY_BALANCED_POWER_ACCURACY) {
+      request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    } else if (priority == LocationEnginePriority.PRIORITY_HIGH_ACCURACY) {
+      request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    //noinspection MissingPermission
+    LocationServices.FusedLocationApi.requestLocationUpdates(googleApiClient, request, this);
+  }
+
+  @Override
+  public void removeLocationUpdates() {
+    LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    for (LocationEngineListener listener : this.locationListeners) {
+      listener.onLocationChanged(location);
+    }
+  }
+}
---
+++ mapbox/app/src/main/java/com/mapbox/services/android/testapp/location/LostLocationEngine.java
@@ -0,0 +1,157 @@
+package com.mapbox.services.android.testapp.location;
+
+import android.content.Context;
+import android.location.Location;
+import android.util.Log;
+
+import com.mapbox.services.android.telemetry.location.LocationEngine;
+import com.mapbox.services.android.telemetry.location.LocationEngineListener;
+import com.mapbox.services.android.telemetry.location.LocationEnginePriority;
+import com.mapbox.services.android.telemetry.permissions.PermissionsManager;
+import com.mapzen.android.lost.api.LocationListener;
+import com.mapzen.android.lost.api.LocationRequest;
+import com.mapzen.android.lost.api.LocationServices;
+import com.mapzen.android.lost.api.LostApiClient;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * Sample LocationEngine using the Open Source Lost library
+ */
+public class LostLocationEngine implements
+  LocationEngine, LostApiClient.ConnectionCallbacks, LocationListener {
+
+  private static final String LOG_TAG = LostLocationEngine.class.getSimpleName();
+
+  private static LocationEngine instance;
+
+  private Context context;
+  private CopyOnWriteArrayList<LocationEngineListener> locationListeners;
+  private LostApiClient lostApiClient;
+
+  private int priority;
+
+  public LostLocationEngine(Context context) {
+    this.context = context;
+    locationListeners = new CopyOnWriteArrayList<>();
+    lostApiClient = new LostApiClient.Builder(context)
+      .addConnectionCallbacks(this)
+      .build();
+  }
+
+  public static LocationEngine getLocationEngine(Context context) {
+    if (instance == null) {
+      instance = new LostLocationEngine(context.getApplicationContext());
+    }
+
+    return instance;
+  }
+
+  @Override
+  public void activate() {
+    if (lostApiClient != null && !lostApiClient.isConnected()) {
+      lostApiClient.connect();
+    }
+  }
+
+  @Override
+  public void deactivate() {
+    if (lostApiClient != null && lostApiClient.isConnected()) {
+      lostApiClient.disconnect();
+    }
+  }
+
+  @Override
+  public boolean isConnected() {
+    return lostApiClient.isConnected();
+  }
+
+  @Override
+  public void onConnected() {
+    for (LocationEngineListener listener : this.locationListeners) {
+      listener.onConnected();
+    }
+  }
+
+  @Override
+  public void onConnectionSuspended() {
+    Log.d(LOG_TAG, "Connection suspended.");
+  }
+
+  @Override
+  public int getPriority() {
+    return priority;
+  }
+
+  @Override
+  public void setPriority(int priority) {
+    this.priority = priority;
+  }
+
+  @Override
+  public Location getLastLocation() {
+    if (lostApiClient.isConnected()
+      && PermissionsManager.isPermissionGranted(context, PermissionsManager.FINE_LOCATION_PERMISSION)) {
+      //noinspection MissingPermission
+      return LocationServices.FusedLocationApi.getLastLocation(lostApiClient);
+    }
+
+    return null;
+  }
+
+  @Override
+  public void addLocationEngineListener(LocationEngineListener listener) {
+    if (!this.locationListeners.contains(listener)) {
+      this.locationListeners.add(listener);
+    }
+  }
+
+  @Override
+  public boolean removeLocationEngineListener(LocationEngineListener listener) {
+    return this.locationListeners.remove(listener);
+  }
+
+  @Override
+  public void requestLocationUpdates() {
+    // Common params
+    LocationRequest request = LocationRequest.create()
+      .setFastestInterval(1000)
+      .setSmallestDisplacement(3.0f);
+
+    // Priority matching is straightforward
+    if (priority == LocationEnginePriority.PRIORITY_NO_POWER) {
+      request.setPriority(LocationRequest.PRIORITY_NO_POWER);
+    } else if (priority == LocationEnginePriority.PRIORITY_LOW_POWER) {
+      request.setPriority(LocationRequest.PRIORITY_LOW_POWER);
+    } else if (priority == LocationEnginePriority.PRIORITY_BALANCED_POWER_ACCURACY) {
+      request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+    } else if (priority == LocationEnginePriority.PRIORITY_HIGH_ACCURACY) {
+      request.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    //noinspection MissingPermission
+    LocationServices.FusedLocationApi.requestLocationUpdates(lostApiClient, request, this);
+  }
+
+  @Override
+  public void removeLocationUpdates() {
+    LocationServices.FusedLocationApi.removeLocationUpdates(lostApiClient, this);
+  }
+
+  @Override
+  public void onLocationChanged(Location location) {
+    for (LocationEngineListener listener : this.locationListeners) {
+      listener.onLocationChanged(location);
+    }
+  }
+
+  @Override
+  public void onProviderDisabled(String provider) {
+    Log.d(LOG_TAG, "Provider disabled: " + provider);
+  }
+
+  @Override
+  public void onProviderEnabled(String provider) {
+    Log.d(LOG_TAG, "Provider enabled: " + provider);
+  }
+}
