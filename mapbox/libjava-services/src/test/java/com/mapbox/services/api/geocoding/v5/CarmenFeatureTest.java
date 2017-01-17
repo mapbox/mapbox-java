0f2ca27a50fb6f8cb729a9052359aeff23e8dc1f
Cameron Mace <cameron@mapbox.com>
2017-01-17 13:58:22 -0500

    added fromjson test and method for carmen feature

--- mapbox/libjava-services/src/test/java/com/mapbox/services/api/geocoding/v5/CarmenFeatureTest.java
+++ mapbox/libjava-services/src/test/java/com/mapbox/services/api/geocoding/v5/CarmenFeatureTest.java
@@ -5,11 +5,17 @@ import com.mapbox.services.api.geocoding.v5.models.CarmenFeature;
 
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import static org.junit.Assert.assertEquals;
 
 public class CarmenFeatureTest {
 
   private static final double DELTA = 1E-10;
+  private static final String GEOCODING_FIXTURE = "src/test/fixtures/geocoder_tofromjson.json";
 
   @Test
   public void testAllowStandalone() {
@@ -49,5 +55,34 @@ public class CarmenFeatureTest {
     assertEquals(feature.hasNonNullValueForProperty("foo1"), true);
     assertEquals(feature.hasNonNullValueForProperty("foo2"), false);
     assertEquals(feature.getId(), "Text field 4");
+  }
+
+  @Test
+  public void carmenFeatureFromJson() throws IOException {
+    // Build the standalone object
+    double[] bbox = new double[] {1.2, 3.4, 5.6, 7.8};
+    double[] center = new double[] {1.2, 3.4};
+    double relevance = 1.2;
+    JsonObject properties = new JsonObject();
+    properties.addProperty("foo1", "bar");
+    CarmenFeature feature = new CarmenFeature();
+
+    // Type specific
+    feature.setText("Text field 1");
+    feature.setPlaceName("Text field 2");
+    feature.setBbox(bbox);
+    feature.setAddress("Text field 3");
+    feature.setCenter(center);
+    feature.setContext(null);
+    feature.setRelevance(relevance);
+
+    // Inherited
+    feature.setGeometry(null);
+    feature.setProperties(properties);
+    feature.setId("Text field 4");
+
+    String body = new String(Files.readAllBytes(Paths.get(GEOCODING_FIXTURE)), Charset.forName("utf-8"));
+    CarmenFeature carmenFeature = CarmenFeature.fromJson(body);
+    assertEquals(carmenFeature.getText(), feature.getText());
   }
 }
