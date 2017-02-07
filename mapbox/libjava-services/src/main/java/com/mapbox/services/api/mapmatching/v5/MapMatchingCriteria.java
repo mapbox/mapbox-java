040d4a6a2516986a364d9f7fef2dd2b8390844cc
Antonio Zugaldia <antonio@mapbox.com>
2017-02-07 16:48:55 -0500

    delete v5 and update client to v5

--- mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/MapMatchingCriteria.java
+++ mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/MapMatchingCriteria.java
@@ -1,4 +1,4 @@
-package com.mapbox.services.api.mapmatching.v4;
+package com.mapbox.services.api.mapmatching.v5;
 
 /**
  * Constant used for the MapMatching request.
@@ -6,26 +6,29 @@ package com.mapbox.services.api.mapmatching.v4;
  * @since 2.0.0
  */
 public class MapMatchingCriteria {
+
+  public static final String PROFILE_DEFAULT_USER = "mapbox";
+
   /**
    * Matches trace to the road network.
    *
    * @since 2.0.0
    */
-  public static final String PROFILE_DRIVING = "mapbox.driving";
+  public static final String PROFILE_DRIVING = "driving";
 
   /**
    * Matches trace to pedestrian and hiking routing.
    *
    * @since 2.0.0
    */
-  public static final String PROFILE_WALKING = "mapbox.walking";
+  public static final String PROFILE_WALKING = "walking";
 
   /**
    * Matches trace to bicycle routing.
    *
    * @since 2.0.0
    */
-  public static final String PROFILE_CYCLING = "mapbox.cycling";
+  public static final String PROFILE_CYCLING = "cycling";
 
   /**
    * The geometry returned will be in the GeoJSON format.
@@ -35,18 +38,39 @@ public class MapMatchingCriteria {
   public static final String GEOMETRY_GEOJSON = "geojson";
 
   /**
-   * The geometry returned will be an encoded polyline with precision 6.
+   * The geometry returned will be an encoded polyline with precision 5.
    *
    * @since 2.0.0
    */
   public static final String GEOMETRY_POLYLINE = "polyline";
 
   /**
-   * Use false to omit geometry from response.
+   * The geometry returned will be an encoded polyline with precision 6.
    *
    * @since 2.0.0
    */
-  public static final String GEOMETRY_FALSE = "false";
+  public static final String GEOMETRY_POLYLINE_6 = "polyline6";
+
+  /**
+   * A simplified version of the {@link #OVERVIEW_FULL} geometry. If not specified simplified is the default.
+   *
+   * @since 2.0.0
+   */
+  public static final String OVERVIEW_SIMPLIFIED = "simplified";
+
+  /**
+   * The most detailed geometry available.
+   *
+   * @since 2.0.0
+   */
+  public static final String OVERVIEW_FULL = "full";
+
+  /**
+   * No overview geometry.
+   *
+   * @since 2.0.0
+   */
+  public static final String OVERVIEW_FALSE = "false";
 
   /**
    * Normal case
