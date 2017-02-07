f02d6390eded36d3da4d0ed741c178e9003900c3
Antonio Zugaldia <antonio@mapbox.com>
2017-02-07 16:47:28 -0500

    include new models for v5

--- mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/models/MapMatchingMatching.java
+++ mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/models/MapMatchingMatching.java
@@ -1,8 +1,29 @@
 package com.mapbox.services.api.mapmatching.v5.models;
 
-/**
- * Created by antonio on 2/7/17.
- */
+import com.mapbox.services.api.directions.v5.models.DirectionsRoute;
 
-public class MapMatchingMatching {
+/**
+ * A match object is a {@link DirectionsRoute} object with an additional confidence field.
+ */
+public class MapMatchingMatching extends DirectionsRoute {
+
+  private double confidence;
+
+  /**
+   * A number between 0 (low) and 1 (high) indicating level of confidence in the returned match
+   *
+   * @return confidence value
+   */
+  public double getConfidence() {
+    return confidence;
+  }
+
+  /**
+   * Set the confidence value
+   *
+   * @param confidence value
+   */
+  public void setConfidence(double confidence) {
+    this.confidence = confidence;
+  }
 }
--- mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/models/MapMatchingTracepoint.java
+++ mapbox/libjava-services/src/main/java/com/mapbox/services/api/mapmatching/v5/models/MapMatchingTracepoint.java
@@ -1,8 +1,52 @@
 package com.mapbox.services.api.mapmatching.v5.models;
 
-/**
- * Created by antonio on 2/7/17.
- */
+import com.google.gson.annotations.SerializedName;
+import com.mapbox.services.api.directions.v5.models.DirectionsWaypoint;
 
-public class MapMatchingTracepoint {
+/**
+ * A tracepoint object is {@link DirectionsWaypoint} object with two additional fields.
+ */
+public class MapMatchingTracepoint extends DirectionsWaypoint {
+
+  @SerializedName("matchings_index")
+  private int matchingsIndex;
+
+  @SerializedName("waypoint_index")
+  private int waypointIndex;
+
+  /**
+   * Index to the match object in matchings the sub-trace was matched to.
+   *
+   * @return index value
+   */
+  public int getMatchingsIndex() {
+    return matchingsIndex;
+  }
+
+  /**
+   * Index value
+   *
+   * @param matchingsIndex value
+   */
+  public void setMatchingsIndex(int matchingsIndex) {
+    this.matchingsIndex = matchingsIndex;
+  }
+
+  /**
+   * Index of the waypoint inside the matched route.
+   *
+   * @return index value
+   */
+  public int getWaypointIndex() {
+    return waypointIndex;
+  }
+
+  /**
+   * Index value
+   *
+   * @param waypointIndex value
+   */
+  public void setWaypointIndex(int waypointIndex) {
+    this.waypointIndex = waypointIndex;
+  }
 }
