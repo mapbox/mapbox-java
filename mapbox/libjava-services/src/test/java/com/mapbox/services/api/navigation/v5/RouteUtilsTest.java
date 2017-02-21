4d0a6c383ce2ab496589b9ecd2bb15468ed05b75
Antonio Zugaldia <antonio@mapbox.com>
2017-02-21 14:48:02 -0500

    change Constants.OSRM_PRECISION_V5 to Constants.PRECISION_6 in tests

--- mapbox/libjava-services/src/test/java/com/mapbox/services/api/navigation/v5/RouteUtilsTest.java
+++ mapbox/libjava-services/src/test/java/com/mapbox/services/api/navigation/v5/RouteUtilsTest.java
@@ -53,7 +53,7 @@ public class RouteUtilsTest extends BaseTest {
     // For each step, the first coordinate is in the step
     for (int stepIndex = 0; stepIndex < route.getSteps().size(); stepIndex++) {
       LegStep step = route.getSteps().get(stepIndex);
-      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.OSRM_PRECISION_V5);
+      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.PRECISION_6);
       assertTrue(routeUtils.isInStep(coords.get(0), route, stepIndex));
     }
   }
@@ -65,7 +65,7 @@ public class RouteUtilsTest extends BaseTest {
     // For each step, the distance to the first coordinate is zero
     for (int stepIndex = 0; stepIndex < route.getSteps().size(); stepIndex++) {
       LegStep step = route.getSteps().get(stepIndex);
-      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.OSRM_PRECISION_V5);
+      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.PRECISION_6);
       assertEquals(0.0, routeUtils.getDistanceToStep(coords.get(0), route, stepIndex), DELTA);
     }
   }
@@ -77,7 +77,7 @@ public class RouteUtilsTest extends BaseTest {
     // For each step, the first coordinate snap point is the same point
     for (int stepIndex = 0; stepIndex < route.getSteps().size(); stepIndex++) {
       LegStep step = route.getSteps().get(stepIndex);
-      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.OSRM_PRECISION_V5);
+      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.PRECISION_6);
       Position snapPoint = routeUtils.getSnapToRoute(coords.get(0), route, stepIndex);
       assertEquals(coords.get(0).getLatitude(), snapPoint.getLatitude(), DELTA);
       assertEquals(coords.get(0).getLongitude(), snapPoint.getLongitude(), DELTA);
@@ -91,14 +91,14 @@ public class RouteUtilsTest extends BaseTest {
     // For each step, the first coordinate is not off-route
     for (int stepIndex = 0; stepIndex < route.getSteps().size(); stepIndex++) {
       LegStep step = route.getSteps().get(stepIndex);
-      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.OSRM_PRECISION_V5);
+      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.PRECISION_6);
       assertFalse(routeUtils.isOffRoute(coords.get(0), route));
     }
 
     // The route goes from SF (N) to San Jose (S). So 0.1 km south of the last point should
     // be off-route
     LegStep lastStep = route.getSteps().get(route.getSteps().size() - 1);
-    List<Position> lastCoords = PolylineUtils.decode(lastStep.getGeometry(), Constants.OSRM_PRECISION_V5);
+    List<Position> lastCoords = PolylineUtils.decode(lastStep.getGeometry(), Constants.PRECISION_6);
     Position offRoutePoint = TurfMeasurement.destination(
       lastCoords.get(0), routeUtils.getOffRouteThresholdKm(), 180, TurfConstants.UNIT_DEFAULT);
     assertTrue(routeUtils.isOffRoute(offRoutePoint, route));
@@ -111,7 +111,7 @@ public class RouteUtilsTest extends BaseTest {
     // For each step, the first coordinate is closest to its step
     for (int stepIndex = 0; stepIndex < route.getSteps().size(); stepIndex++) {
       LegStep step = route.getSteps().get(stepIndex);
-      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.OSRM_PRECISION_V5);
+      List<Position> coords = PolylineUtils.decode(step.getGeometry(), Constants.PRECISION_6);
       assertEquals(stepIndex, routeUtils.getClosestStep(coords.get(0), route));
     }
   }
