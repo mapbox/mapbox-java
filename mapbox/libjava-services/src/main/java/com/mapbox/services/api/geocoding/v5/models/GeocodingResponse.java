7a6157c004b19d070039827758b108038b0bf7ee
Antonio Zugaldia <antonio@mapbox.com>
2017-02-27 09:26:35 -0500

    more missing no-args constructors (#363)

--- mapbox/libjava-services/src/main/java/com/mapbox/services/api/geocoding/v5/models/GeocodingResponse.java
+++ mapbox/libjava-services/src/main/java/com/mapbox/services/api/geocoding/v5/models/GeocodingResponse.java
@@ -9,6 +9,9 @@ import java.util.List;
  */
 public class GeocodingResponse extends CarmenFeatureCollection {
 
+  public GeocodingResponse() {
+  }
+
   public GeocodingResponse(List<CarmenFeature> features) {
     super(features);
   }
