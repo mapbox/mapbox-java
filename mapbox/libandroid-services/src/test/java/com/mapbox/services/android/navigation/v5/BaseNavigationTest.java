69ddabc53dc6d425005aec2c4761c75f5dc982a8
Cameron Mace <cameron@mapbox.com>
2017-03-02 10:02:26 -0500

    added test to PR

--- mapbox/libandroid-services/src/test/java/com/mapbox/services/android/navigation/v5/BaseNavigationTest.java
+++ mapbox/libandroid-services/src/test/java/com/mapbox/services/android/navigation/v5/BaseNavigationTest.java
@@ -1,28 +1,25 @@
 package com.mapbox.services.android.navigation.v5;
 
-import android.content.Context;
-
 import java.io.IOException;
 import java.io.InputStream;
-
+import java.io.InputStreamReader;
+import java.io.Reader;
 
 public class BaseNavigationTest {
 
-  public static String readFileFromAssets(String fileName, Context c) {
-    try {
-      InputStream is = c.getAssets().open(fileName);
-      int size = is.available();
-      byte[] buffer = new byte[size];
-      is.read(buffer);
-      is.close();
-      String text = new String(buffer);
-
-      return text;
-
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+  static String convertStreamToString(InputStream is) throws IOException {
+    final int bufferSize = 1024;
+    final char[] buffer = new char[bufferSize];
+    final StringBuilder out = new StringBuilder();
+    Reader in = new InputStreamReader(is, "UTF-8");
+    for (; ; ) {
+      int rsz = in.read(buffer, 0, buffer.length);
+      if (rsz < 0) {
+        break;
+      }
+      out.append(buffer, 0, rsz);
     }
-
+    return out.toString();
   }
 
 }
--- mapbox/libandroid-services/src/test/java/com/mapbox/services/android/navigation/v5/MapboxNavigationTest.java
+++ mapbox/libandroid-services/src/test/java/com/mapbox/services/android/navigation/v5/MapboxNavigationTest.java
@@ -2,8 +2,6 @@ package com.mapbox.services.android.navigation.v5;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class MapboxNavigationTest {
 
   @Test
