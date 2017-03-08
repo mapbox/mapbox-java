36efbdee8ec1d9cd1c1d3ef3b3dab973306d67c2
Antonio Zugaldia <antonio@mapbox.com>
2017-03-08 10:06:03 -0800

    fix checkstyle

--- mapbox/libjava-services/src/main/java/com/mapbox/services/api/navigation/v5/osrm/TextInstructions.java
+++ mapbox/libjava-services/src/main/java/com/mapbox/services/api/navigation/v5/osrm/TextInstructions.java
@@ -263,11 +263,13 @@ public class TextInstructions {
     // Replace tokens
     // NOOP if they don't exist
     String nthWaypoint = ""; // TODO, add correct waypoint counting
-    JsonPrimitive modifierValue = getVersionObject().getAsJsonObject("constants").getAsJsonObject("modifier").getAsJsonPrimitive(modifier);
+    JsonPrimitive modifierValue =
+      getVersionObject().getAsJsonObject("constants").getAsJsonObject("modifier").getAsJsonPrimitive(modifier);
     instruction = instruction
       .replace("{way_name}", wayName)
       .replace("{destination}", TextUtils.isEmpty(step.getDestinations()) ? "" : step.getDestinations().split(",")[0])
-      .replace("{exit_number}", step.getManeuver().getExit() == null ? ordinalize(1) : ordinalize(step.getManeuver().getExit()))
+      .replace("{exit_number}",
+        step.getManeuver().getExit() == null ? ordinalize(1) : ordinalize(step.getManeuver().getExit()))
       .replace("{rotary_name}", TextUtils.isEmpty(step.getRotaryName()) ? "" : step.getRotaryName())
       .replace("{lane_instruction}", laneInstruction == null ? "" : laneInstruction.getAsString())
       .replace("{modifier}", modifierValue == null ? "" : modifierValue.getAsString())
