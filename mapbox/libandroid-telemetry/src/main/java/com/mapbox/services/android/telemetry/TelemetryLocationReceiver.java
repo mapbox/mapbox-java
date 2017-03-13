5a733468ad41c0da66b5cc2006ea5668e7004bca
Antonio Zugaldia <antonio@mapbox.com>
2017-03-13 15:20:17 -0700

    checkstyle once again

--- mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/TelemetryLocationReceiver.java
+++ mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/TelemetryLocationReceiver.java
@@ -5,7 +5,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
-import android.util.Log;
 
 /**
  * The TelemetryService will register for updates sent to this TelemetryLocationReceiver.
