0b731fc45e5626ec241a14b6996de7e7e47f3dbe
Guardiola31337 <pablo.guardiola@mapbox.com>
2017-10-04 20:12:18 +0200

    ignore location engine coming from mapbox-android-sdk momentarily so that CI passes (chicken-egg problem)

--- mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/MapboxTelemetry.java
+++ mapbox/libandroid-telemetry/src/main/java/com/mapbox/services/android/telemetry/MapboxTelemetry.java
@@ -128,11 +128,11 @@ public class MapboxTelemetry implements Callback, LocationEngineListener {
   /**
    * Initialize MapboxTelemetry - with sdkIdentifier + sdkVersion
    *
-   * @param context        The context associated with the application
-   * @param accessToken    The accessToken associated with the application
-   * @param userAgent      source of requests
-   * @param sdkIdentifier  Identifies which sdk is sending the event
-   * @param sdkVersion     version of the sdk sending the event
+   * @param context       The context associated with the application
+   * @param accessToken   The accessToken associated with the application
+   * @param userAgent     source of requests
+   * @param sdkIdentifier Identifies which sdk is sending the event
+   * @param sdkVersion    version of the sdk sending the event
    */
 
   public void initialize(@NonNull Context context, @NonNull String accessToken, @NonNull String userAgent,
@@ -476,10 +476,11 @@ public class MapboxTelemetry implements Callback, LocationEngineListener {
   }
 
   private void registerLocationUpdates() {
-    if (locationEngine == null) {
-      LocationEngineProvider locationEngineProvider = new LocationEngineProvider(context);
-      locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
-    }
+    // TODO This has to be uncommented when https://github.com/mapbox/mapbox-gl-native/pull/9488 lands
+    // if (locationEngine == null) {
+    LocationEngineProvider locationEngineProvider = new LocationEngineProvider(context);
+    locationEngine = locationEngineProvider.obtainBestLocationEngineAvailable();
+    // }
 
     locationEngine.addLocationEngineListener(this);
     locationEngine.activate();
