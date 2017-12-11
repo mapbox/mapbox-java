d08578f481d5ecca3bea7f8bc931a9b87a9bb648
Cameron Mace <cameron@mapbox.com>
2017-12-11 14:27:05 -0500

    Adds annotations and test for polylineUtils

--- services-geojson/src/test/java/com/mapbox/geojson/utils/PolylineUtilsTest.java
+++ services-geojson/src/test/java/com/mapbox/geojson/utils/PolylineUtilsTest.java
@@ -1,12 +1,17 @@
 package com.mapbox.geojson.utils;
 
-import com.mapbox.core.constants.Constants;
-import com.mapbox.core.TestUtils;
-import com.mapbox.geojson.Point;
+import static com.mapbox.geojson.utils.PolylineUtils.decode;
+import static com.mapbox.geojson.utils.PolylineUtils.encode;
+import static com.mapbox.geojson.utils.PolylineUtils.simplify;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
-import org.junit.Assert;
+import com.mapbox.core.TestUtils;
+import com.mapbox.core.constants.Constants;
+import com.mapbox.geojson.Point;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PolylineUtilsTest extends TestUtils {
@@ -15,10 +20,10 @@ public class PolylineUtilsTest extends TestUtils {
 
   @Test
   public void testDecodePath() {
-    List<Point> latLngs = PolylineUtils.decode(TEST_LINE, Constants.PRECISION_5);
+    List<Point> latLngs = decode(TEST_LINE, Constants.PRECISION_5);
 
     int expectedLength = 21;
-    Assert.assertEquals("Wrong length.", expectedLength, latLngs.size());
+    assertEquals("Wrong length.", expectedLength, latLngs.size());
 
     Point lastPoint = latLngs.get(expectedLength - 1);
     expectNearNumber(37.76953, lastPoint.latitude(), 1e-6);
@@ -27,8 +32,27 @@ public class PolylineUtilsTest extends TestUtils {
 
   @Test
   public void testEncodePath() {
-    List<Point> path = PolylineUtils.decode(TEST_LINE, Constants.PRECISION_5);
-    String encoded = PolylineUtils.encode(path, Constants.PRECISION_5);
-    Assert.assertEquals(TEST_LINE, encoded);
+    List<Point> path = decode(TEST_LINE, Constants.PRECISION_5);
+    String encoded = encode(path, Constants.PRECISION_5);
+    assertEquals(TEST_LINE, encoded);
+  }
+
+  @Test
+  public void decode_neverReturnsNullButRatherAnEmptyList() throws Exception {
+    List<Point> path = decode("", Constants.PRECISION_5);
+    assertNotNull(path);
+    assertEquals(0, path.size());
+  }
+
+  @Test
+  public void encode_neverReturnsNull() throws Exception {
+    String encodedString = encode(new ArrayList<Point>(), Constants.PRECISION_6);
+    assertNotNull(encodedString);
+  }
+
+  @Test
+  public void simplify_neverReturnsNullButRatherAnEmptyList() throws Exception {
+    List<Point> simplifiedPath = simplify(new ArrayList<Point>(), Constants.PRECISION_6);
+    assertNotNull(simplifiedPath);
   }
 }
