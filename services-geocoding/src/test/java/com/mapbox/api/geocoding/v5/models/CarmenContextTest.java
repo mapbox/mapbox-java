c4286b674cc8845b0632aa35919e25245f0319b4
Cameron Mace <cameron@mapbox.com>
2018-01-18 10:41:48 -0500

    Cleaned up context test class

--- services-geocoding/src/test/java/com/mapbox/api/geocoding/v5/models/CarmenContextTest.java
+++ services-geocoding/src/test/java/com/mapbox/api/geocoding/v5/models/CarmenContextTest.java
@@ -1,26 +1,20 @@
 package com.mapbox.api.geocoding.v5.models;
 
+import static org.hamcrest.Matchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 import com.mapbox.api.geocoding.v5.MapboxGeocoding;
 import com.mapbox.core.TestUtils;
 import okhttp3.HttpUrl;
 import okhttp3.mockwebserver.MockWebServer;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import retrofit2.Response;
 
 public class CarmenContextTest extends TestUtils {
-
-  private static final String GEOCODING_FIXTURE = "geocoding.json";
-  private static final String GEOCODING_BATCH_FIXTURE = "geocoding_batch.json";
-  private static final String REVERSE_GEOCODE_FIXTURE = "geocoding_reverse.json";
-  private static final String GEOCODE_WITH_BBOX_FIXTURE = "bbox_geocoding_result.json";
-  private static final String GEOCODE_LANGUAGE_FIXTURE = "language_geocoding_result.json";
-
-  private MockWebServer server;
-  private HttpUrl mockUrl;
 
   @Test
   public void sanity() throws Exception {
@@ -35,9 +29,9 @@ public class CarmenContextTest extends TestUtils {
       .query("1600 pennsylvania ave nw")
       .baseUrl(mockUrl.toString())
       .build();
-    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-    assertTrue(response.body().features().get(0).context().get(0).id()
-      .equals("neighborhood.291451"));
+    GeocodingResponse response = mapboxGeocoding.executeCall().body();
+    assert response != null;
+    assertThat(response.features().get(0).context().get(0).id(), equalTo("neighborhood.291451"));
   }
 
   @Test
@@ -47,21 +41,21 @@ public class CarmenContextTest extends TestUtils {
       .query("1600 pennsylvania ave nw")
       .baseUrl(mockUrl.toString())
       .build();
-    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-    assertTrue(response.body().features().get(0).context().get(0).text()
-      .equals("Downtown"));
+    GeocodingResponse response = mapboxGeocoding.executeCall().body();
+    assert response != null;
+    assertThat(response.features().get(0).context().get(0).text(), equalTo("Downtown"));
   }
 
   @Test
   public void shortCode_returnsCorrectString() throws Exception {
     MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
       .accessToken(ACCESS_TOKEN)
-      .query("texas")
+      .query("1600 pennsylvania ave nw")
       .baseUrl(mockUrl.toString())
       .build();
-    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-    assertTrue(response.body().features().get(0).context().get(0).shortCode()
-      .equals("us"));
+    GeocodingResponse response = mapboxGeocoding.executeCall().body();
+    assert response != null;
+    assertThat(response.features().get(0).context().get(3).shortCode(), equalTo("US-DC"));
   }
 
   @Test
@@ -71,38 +65,10 @@ public class CarmenContextTest extends TestUtils {
       .query("texas")
       .baseUrl(mockUrl.toString())
       .build();
-    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-    assertTrue(response.body().features().get(0).context().get(0).wikidata()
-      .equals("Q30"));
+    GeocodingResponse response = mapboxGeocoding.executeCall().body();
+    assert response != null;
+    assertThat(response.features().get(0).context().get(2).wikidata(), equalTo("Q148"));
   }
-
-//  @Test
-//  public void category_returnsCorrectString() throws Exception {
-  // TODO find a fixture with category
-//    MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
-//      .accessToken(ACCESS_TOKEN)
-//      .query("1600 pennsylvania ave nw")
-//      .baseUrl(mockUrl.toString())
-//      .build();
-//    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-//    System.out.println(response.body().features().get(0).context().get(0).category());
-//    assertTrue(response.body().features().get(0).context().get(0).category()
-//      .equals("Q30"));
-//  }
-
-//  @Test
-//  public void maki_returnsCorrectString() throws Exception {
-//    // TODO find a fixture with category
-//    MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
-//      .accessToken(ACCESS_TOKEN)
-//      .query("texas")
-//      .baseUrl(mockUrl.toString())
-//      .build();
-//    Response<GeocodingResponse> response = mapboxGeocoding.executeCall();
-//    System.out.println(response.body().features().get(0).context().get(0).maki());
-//    assertTrue(response.body().features().get(0).context().get(0).maki()
-//      .equals("Q30"));
-//  }
 
   @Test
   public void testSerializable() throws Exception {
