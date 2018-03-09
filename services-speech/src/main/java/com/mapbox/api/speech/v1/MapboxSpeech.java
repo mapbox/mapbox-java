27fee0aee209a6cf8867c98cff45dd066fe3a962
Devota Aabel <devota.aabel@mapbox.com>
2018-03-09 15:25:54 -0500

    Added API voice interface and MapboxSpeech class

---
+++ services-speech/src/main/java/com/mapbox/api/speech/v1/MapboxSpeech.java
@@ -0,0 +1,117 @@
+package com.mapbox.api.speech.v1;
+
+
+import com.google.auto.value.AutoValue;
+import com.sun.istack.internal.Nullable;
+
+import java.io.File;
+
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+import static com.mapbox.core.constants.Constants.BASE_API_URL;
+
+@AutoValue
+public abstract class MapboxSpeech {
+  public void getInstruction(String instruction, Callback<ResponseBody> callback) {
+    voiceService().getInstruction(
+      instruction, textType(),
+      language(),
+      outputType(),
+      accessToken()).enqueue(callback);
+  }
+
+  public void cacheInstruction(String instruction) {
+    getInstruction(instruction, new Callback<ResponseBody>() {
+      @Override
+      public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+
+      }
+
+      @Override
+      public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+
+      }
+    });
+  }
+
+  @Nullable
+  abstract String language();
+
+  @Nullable
+  abstract String textType();
+
+  @Nullable
+  abstract String outputType();
+
+  abstract String accessToken();
+
+  abstract VoiceService voiceService();
+
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    long cacheSize;
+    File cacheDirectory;
+
+    public abstract Builder language(String language);
+
+    public abstract Builder textType(String textType);
+
+    public abstract Builder outputType(String outputType);
+
+    public abstract Builder accessToken(String accessToken);
+
+    public Builder cacheSize(long cacheSize) {
+      this.cacheSize = cacheSize;
+      return this;
+    }
+
+    long cacheSize() {
+      return cacheSize;
+    }
+
+    abstract Builder voiceService(VoiceService voiceService);
+
+    public Builder cacheDirectory(File cacheDirectory) {
+      this.cacheDirectory = cacheDirectory;
+      return this;
+    }
+
+    File cacheDirectory() {
+      return cacheDirectory;
+    }
+
+    abstract MapboxSpeech autoBuild();
+
+    public MapboxSpeech build() {
+      voiceService(getVoiceService());
+      return autoBuild();
+    }
+
+    private VoiceService getVoiceService() {
+      Cache cache = new Cache(cacheDirectory(), cacheSize());
+
+      OkHttpClient okHttpClient = new OkHttpClient.Builder()
+        .cache(cache)
+        .build();
+
+      Retrofit retrofit = new Retrofit.Builder()
+        .baseUrl(BASE_API_URL)
+        .client(okHttpClient)
+        .build();
+
+      return retrofit.create(VoiceService.class);
+    }
+  }
+
+  public static Builder builder() {
+    return new AutoValue_MapboxSpeech.Builder()
+      .cacheSize(10 * 1024 * 1024); // default cache size is 10 MB
+  }
+}
---
+++ services-speech/src/main/java/com/mapbox/api/speech/v1/VoiceService.java
@@ -0,0 +1,17 @@
+package com.mapbox.api.speech.v1;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface VoiceService {
+  @GET("/voice/v1/speak/{text}")
+  Call<ResponseBody> getInstruction(
+    @Path("text") String text,
+    @Query("textType") String textType,
+    @Query("language") String language,
+    @Query("outputFormat") String outputFormat,
+    @Query("access_token") String accessToken);
+}
