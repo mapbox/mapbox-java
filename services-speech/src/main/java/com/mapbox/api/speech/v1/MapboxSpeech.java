4943c6cbc6dbe46fe0f33f04fd66f7330a751c86
Devota Aabel <devota.aabel@mapbox.com>
2018-03-12 13:43:32 -0400

    Added javadoc

--- services-speech/src/main/java/com/mapbox/api/speech/v1/MapboxSpeech.java
+++ services-speech/src/main/java/com/mapbox/api/speech/v1/MapboxSpeech.java
@@ -2,6 +2,7 @@ package com.mapbox.api.speech.v1;
 
 
 import com.google.auto.value.AutoValue;
+import com.mapbox.core.exceptions.ServicesException;
 import com.sun.istack.internal.Nullable;
 
 import java.io.File;
@@ -18,6 +19,13 @@ import static com.mapbox.core.constants.Constants.BASE_API_URL;
 
 @AutoValue
 public abstract class MapboxSpeech {
+  /**
+   * Makes the API call to get the instruction using parameters specified when MapboxSpeech was
+   * built.
+   *
+   * @param instruction text to pass to API voice for dictation
+   * @param callback to alert status updates for API call
+   */
   public void getInstruction(String instruction, Callback<ResponseBody> callback) {
     voiceService().getInstruction(
       instruction, textType(),
@@ -26,6 +34,12 @@ public abstract class MapboxSpeech {
       accessToken()).enqueue(callback);
   }
 
+  /**
+   * Makes the specified instruction call with an empty callback, subsequently caching it for later
+   * use.
+   *
+   * @param instruction text to pass to API voice for dictation
+   */
   public void cacheInstruction(String instruction) {
     getInstruction(instruction, new Callback<ResponseBody>() {
       @Override
@@ -59,14 +73,46 @@ public abstract class MapboxSpeech {
     long cacheSize;
     File cacheDirectory;
 
+    /**
+     * Language of which to request the instructions be spoken
+     *
+     * @param language as a string, i.e., "en-us"
+     * @return this builder for chaining options together
+     */
     public abstract Builder language(String language);
 
+    /**
+     * Format which the input is specified
+     *
+     * @param textType either text or ssml
+     * @return this builder for chaining options together
+     */
     public abstract Builder textType(String textType);
 
+    /**
+     * Output format for spoken instructions
+     *
+     * @param outputType, either mp3, ogg_vorbis or pcm
+     * @return this builder for chaining options together
+     */
     public abstract Builder outputType(String outputType);
 
+    /**
+     * Required to call when this is being built. If no access token provided,
+     * {@link ServicesException} will be thrown.
+     *
+     * @param accessToken Mapbox access token, You must have a Mapbox account in order to use
+     *                    the Optimization API
+     * @return this builder for chaining options together
+     */
     public abstract Builder accessToken(String accessToken);
 
+    /**
+     * Size for the OkHTTP cache
+     *
+     * @param cacheSize in bytes
+     * @return this builder for chaining options together
+     */
     public Builder cacheSize(long cacheSize) {
       this.cacheSize = cacheSize;
       return this;
@@ -78,6 +124,11 @@ public abstract class MapboxSpeech {
 
     abstract Builder voiceService(VoiceService voiceService);
 
+    /**
+     * Directory where to instantiate OkHTTP cache
+     *
+     * @return this builder for chaining options together
+     */
     public Builder cacheDirectory(File cacheDirectory) {
       this.cacheDirectory = cacheDirectory;
       return this;
@@ -89,6 +140,11 @@ public abstract class MapboxSpeech {
 
     abstract MapboxSpeech autoBuild();
 
+    /**
+     * Creates the Retrofit instance and builds a new MapboxSpeech object
+     *
+     * @return MapboxSpeech object with specified parameters
+     */
     public MapboxSpeech build() {
       voiceService(getVoiceService());
       return autoBuild();
@@ -110,6 +166,11 @@ public abstract class MapboxSpeech {
     }
   }
 
+  /**
+   * Creates a builder for a MapboxSpeech object with a default cache size of 10 MB
+   *
+   * @return a builder to create a MapboxSpeech object
+   */
   public static Builder builder() {
     return new AutoValue_MapboxSpeech.Builder()
       .cacheSize(10 * 1024 * 1024); // default cache size is 10 MB
