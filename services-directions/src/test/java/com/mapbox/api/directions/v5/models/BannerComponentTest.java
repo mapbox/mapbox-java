dfca45e2a66db9ae8e8073c3067362662ab5cbe3
Dan Nesfeder <dan.nesfeder@mapbox.com>
2018-03-22 16:04:49 -0400

    Add Abbreviations and type to BannerComponent (#762)
    * Add Abbreviations and type to BannerComponent
    
    * Fix checkstyle line length
    
    * Update tests
    
    * Update component test

--- services-directions/src/test/java/com/mapbox/api/directions/v5/models/BannerComponentTest.java
+++ services-directions/src/test/java/com/mapbox/api/directions/v5/models/BannerComponentTest.java
@@ -1,23 +1,30 @@
 package com.mapbox.api.directions.v5.models;
 
+import com.mapbox.core.TestUtils;
+
+import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
-import com.mapbox.core.TestUtils;
-import org.junit.Test;
 
 public class BannerComponentTest extends TestUtils {
 
   @Test
   public void sanity() throws Exception {
-    BannerComponents bannerComponents = BannerComponents.builder().text("test").build();
+    BannerComponents bannerComponents = BannerComponents.builder()
+      .text("test")
+      .type("text")
+      .build();
     assertNotNull(bannerComponents);
   }
 
   @Test
   public void testSerializable() throws Exception {
-    BannerComponents bannerComponents
-      = BannerComponents.builder().imageBaseUrl("www.test.com").text("test").build();
+    BannerComponents bannerComponents = BannerComponents.builder()
+      .imageBaseUrl("www.test.com")
+      .text("test")
+      .type("icon")
+      .build();
     byte[] serialized = TestUtils.serialize(bannerComponents);
     assertEquals(bannerComponents, deserialize(serialized, BannerComponents.class));
   }
