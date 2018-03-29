642b0396696924676d1ff1cbb5dfc35089a7901b
Ivo van Dongen <ivovandongen@users.noreply.github.com>
2018-03-29 18:35:43 +0300

    Geometry from json (#770)
    * Update java level to 1.8
    
    * [geojson] Add static method to load Geometry form json string

--- services-geojson/src/main/java/com/mapbox/geojson/Geometry.java
+++ services-geojson/src/main/java/com/mapbox/geojson/Geometry.java
@@ -1,5 +1,12 @@
 package com.mapbox.geojson;
 
+import android.support.annotation.NonNull;
+
+import com.google.gson.GsonBuilder;
+import com.mapbox.geojson.gson.GeoJsonAdapterFactory;
+import com.mapbox.geojson.gson.GeometryDeserializer;
+import com.mapbox.geojson.gson.PointDeserializer;
+
 /**
  * Each of the six geometries and {@link GeometryCollection}
  * which make up GeoJson implement this interface.
@@ -7,5 +14,21 @@ package com.mapbox.geojson;
  * @since 1.0.0
  */
 public interface Geometry extends GeoJson {
+
+  /**
+   * Create a new instance of this class by passing in a formatted valid JSON String.
+   *
+   * @param json a formatted valid JSON string defining a GeoJson Geometry
+   * @return a new instance of this class defined by the values passed inside this static factory
+   *   method
+   * @since 3.0.0
+   */
+  static Geometry fromJson(@NonNull String json) {
+    GsonBuilder gson = new GsonBuilder();
+    gson.registerTypeAdapterFactory(GeoJsonAdapterFactory.create());
+    gson.registerTypeAdapter(Point.class, new PointDeserializer());
+    gson.registerTypeAdapter(Geometry.class, new GeometryDeserializer());
+    return gson.create().fromJson(json, Geometry.class);
+  }
 
 }
