2bf86d516c4657e156695e5fbf2c2f4f7641e6c7
Osana Babayan <32496536+osana@users.noreply.github.com>
2018-04-18 11:38:16 -0400

    Fix for #766:Test missing for public static method in PolylineUtils.java (#797)

--- services-geojson/src/test/java/com/mapbox/geojson/utils/PolylineUtilsTest.java
+++ services-geojson/src/test/java/com/mapbox/geojson/utils/PolylineUtilsTest.java
@@ -5,17 +5,21 @@ import static com.mapbox.geojson.utils.PolylineUtils.encode;
 import static com.mapbox.geojson.utils.PolylineUtils.simplify;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import com.mapbox.core.TestUtils;
 import com.mapbox.core.constants.Constants;
 import com.mapbox.geojson.Point;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class PolylineUtilsTest extends TestUtils {
 
+  private static final String SIMPLIFICATION_INPUT = "simplification-input";
+  private static final String SIMPLIFICATION_EXPECTED_OUTPUT = "simplification-expected-output";
   private static final String TEST_LINE
     = "_cqeFf~cjVf@p@fA}AtAoB`ArAx@hA`GbIvDiFv@gAh@t@X\\|@z@`@Z\\Xf@Vf@VpA\\tATJ@NBBkC";
 
@@ -55,5 +59,42 @@ public class PolylineUtilsTest extends TestUtils {
   public void simplify_neverReturnsNullButRatherAnEmptyList() throws Exception {
     List<Point> simplifiedPath = simplify(new ArrayList<Point>(), Constants.PRECISION_6);
     assertNotNull(simplifiedPath);
+  }
+
+  @Test
+  public void simplify_returnSameListWhenListSizeIsLessThanOrEqualToTwo(){
+    final List<Point> path = new ArrayList<>();
+    path.add(Point.fromLngLat(0, 0));
+    path.add(Point.fromLngLat(10, 0));
+    List<Point> simplifiedPath = simplify(path, Constants.PRECISION_6, true);
+    assertTrue("Returned list is different from input list", path == simplifiedPath);
+  }
+
+  @Test
+  public void simplify_withHighestQuality() throws IOException{
+    List<Point> path = createPointListFromResourceFile(SIMPLIFICATION_INPUT);
+    List<Point> simplifiedPath = simplify(path, Constants.PRECISION_5, true);
+    List<Point> expectedSimplifiedPath = createPointListFromResourceFile(SIMPLIFICATION_EXPECTED_OUTPUT);
+    assertTrue("Wrong number of points retained",simplifiedPath.size() == expectedSimplifiedPath.size());
+    int counter = 0;
+    for(Point retainedPoint:simplifiedPath){
+      Point expectedPoint = expectedSimplifiedPath.get(counter);
+      assertTrue("Wrong point retained by simplification algorithm",retainedPoint.equals(expectedPoint));
+      ++counter;
+    }
+  }
+
+  private List<Point> createPointListFromResourceFile(String fileName) throws IOException {
+    String inputPoints = loadJsonFixture(fileName);
+    String[] coords = inputPoints.split(",");
+
+    final List<Point> pointList = new ArrayList<>();
+    for(int i= 0;i <= coords.length-2;i = i+2) {
+      double x = Double.parseDouble(coords[i]);
+      double y = Double.parseDouble(coords[i+1]);
+      pointList.add(Point.fromLngLat(x, y));
+    }
+
+    return pointList;
   }
 }
