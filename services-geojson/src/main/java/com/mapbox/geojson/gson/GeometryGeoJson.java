4d1b2f8b24b65c0829bcaa143e8ec58f0c377752
Osana Babayan <32496536+osana@users.noreply.github.com>
2018-09-19 12:59:32 -0400

    move default method to a utility class so that we can downgrade to java 7 (#876)

---
+++ services-geojson/src/main/java/com/mapbox/geojson/gson/GeometryGeoJson.java
@@ -0,0 +1,30 @@
+package com.mapbox.geojson.gson;
+
+import android.support.annotation.NonNull;
+
+import com.google.gson.GsonBuilder;
+import com.mapbox.geojson.Geometry;
+import com.mapbox.geojson.Point;
+
+/**
+ * This is a utility class that helps create a Geometry instance from a JSON string.
+ * @since 3.5.0
+ */
+public class GeometryGeoJson {
+
+  /**
+   * Create a new instance of Geometry class by passing in a formatted valid JSON String.
+   *
+   * @param json a formatted valid JSON string defining a GeoJson Geometry
+   * @return a new instance of Geometry class defined by the values passed inside
+   *   this static factory method
+   * @since 3.5.0
+   */
+  public static Geometry fromJson(@NonNull String json) {
+    GsonBuilder gson = new GsonBuilder();
+    gson.registerTypeAdapterFactory(GeoJsonAdapterFactory.create());
+    gson.registerTypeAdapter(Point.class, new PointDeserializer());
+    gson.registerTypeAdapter(Geometry.class, new GeometryDeserializer());
+    return gson.create().fromJson(json, Geometry.class);
+  }
+}
