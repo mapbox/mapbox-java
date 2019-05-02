86b0594ec30a15754d9d6cd6b7a54c24d6123346
Langston Smith <langston.smith@mapbox.com>
2019-05-02 11:31:51 -0700

    Adding explode turf method (#1007)

--- services-turf/src/test/java/com/mapbox/turf/TurfConversionTest.java
+++ services-turf/src/test/java/com/mapbox/turf/TurfConversionTest.java
@@ -1,12 +1,31 @@
 package com.mapbox.turf;
 
+import com.mapbox.geojson.Feature;
+import com.mapbox.geojson.FeatureCollection;
+import com.mapbox.geojson.GeometryCollection;
+import com.mapbox.geojson.LineString;
+import com.mapbox.geojson.MultiLineString;
+import com.mapbox.geojson.MultiPoint;
+import com.mapbox.geojson.MultiPolygon;
+import com.mapbox.geojson.Point;
+import com.mapbox.geojson.Polygon;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import java.io.IOException;
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 public class TurfConversionTest extends TestUtils {
+
+  private static final String TURF_EXPLODE_MULTI_POINT = "turf-explode/multipoint.geojson";
+  private static final String TURF_EXPLODE_LINESTRING = "turf-explode/linestring.geojson";
+  private static final String TURF_EXPLODE_MULTILINESTRING = "turf-explode/multilinestring.geojson";
+  private static final String TURF_EXPLODE_MULTIPOLYGON = "turf-explode/multipolygon.geojson";
+  private static final String TURF_EXPLODE_GEOMETRY_COLLECTION = "turf-explode/geometrycollection.geojson";
 
   @Rule
   public ExpectedException thrown = ExpectedException.none();
@@ -57,5 +76,61 @@ public class TurfConversionTest extends TestUtils {
     assertEquals(100,
       TurfConversion.convertLength(1, TurfConstants.UNIT_METERS,
         TurfConstants.UNIT_CENTIMETERS), DELTA);
+  }
+
+  @Test
+  public void explodePointSingleFeature() throws IOException, NullPointerException {
+    Point point = Point.fromLngLat(102, 0.5);
+    assertEquals(1, TurfConversion.explode(Feature.fromGeometry(point)).features().size());
+  }
+
+  @Test
+  public void explodeMultiPointSingleFeature() throws IOException, NullPointerException {
+    MultiPoint multiPoint = MultiPoint.fromJson(loadJsonFixture(TURF_EXPLODE_MULTI_POINT));
+    assertEquals(4, TurfConversion.explode(Feature.fromGeometry(multiPoint)).features().size());
+  }
+
+  @Test
+  public void explodeLineStringSingleFeature() throws IOException, NullPointerException {
+    LineString lineString = LineString.fromJson(loadJsonFixture(TURF_EXPLODE_LINESTRING));
+    assertEquals(4, TurfConversion.explode(Feature.fromGeometry(lineString)).features().size());
+  }
+
+  @Test
+  public void explodePolygonSingleFeature() throws IOException, NullPointerException {
+    Polygon polygon = Polygon.fromLngLats(Arrays.asList(
+      Arrays.asList(
+        Point.fromLngLat(0, 101),
+        Point.fromLngLat(1, 101),
+        Point.fromLngLat(1, 100),
+        Point.fromLngLat(0, 100))));
+    assertEquals(3, TurfConversion.explode(Feature.fromGeometry(polygon)).features().size());
+  }
+
+  @Test
+  public void explodeMultiLineStringSingleFeature() throws IOException, NullPointerException {
+    MultiLineString multiLineString = MultiLineString.fromJson(loadJsonFixture(TURF_EXPLODE_MULTILINESTRING));
+    assertEquals(4, TurfConversion.explode(Feature.fromGeometry(multiLineString)).features().size());
+  }
+
+  @Test
+  public void explodeMultiPolygonSingleFeature() throws IOException, NullPointerException {
+    MultiPolygon multiPolygon = MultiPolygon.fromJson(loadJsonFixture(TURF_EXPLODE_MULTIPOLYGON));
+    assertEquals(12, TurfConversion.explode(Feature.fromGeometry(multiPolygon)).features().size());
+  }
+
+  @Test
+  public void explodeGeometryCollectionSingleFeature() throws IOException, NullPointerException {
+    GeometryCollection geometryCollection = GeometryCollection.fromJson(loadJsonFixture(TURF_EXPLODE_GEOMETRY_COLLECTION));
+    assertEquals(3, TurfConversion.explode(Feature.fromGeometry(geometryCollection)).features().size());
+  }
+
+  @Test
+  public void explodeFeatureCollection() throws IOException, NullPointerException {
+    FeatureCollection featureCollection = FeatureCollection.fromFeatures(new Feature[] {
+      Feature.fromGeometry(MultiLineString.fromJson(loadJsonFixture(TURF_EXPLODE_MULTILINESTRING))),
+      Feature.fromGeometry(MultiPolygon.fromJson(loadJsonFixture(TURF_EXPLODE_MULTIPOLYGON)))
+    });
+    assertEquals(16, TurfConversion.explode(featureCollection).features().size());
   }
 }
