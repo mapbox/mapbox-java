bfd5a9300f03a7185cf5e71614582179f6597566
Kyle Madsen <>
2020-01-07 14:23:57 -0800

    style fails

--- services-directions/src/main/java/com/mapbox/api/directions/v5/models/BannerInstructions.java
+++ services-directions/src/main/java/com/mapbox/api/directions/v5/models/BannerInstructions.java
@@ -2,6 +2,7 @@ package com.mapbox.api.directions.v5.models;
 
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+
 import com.google.auto.value.AutoValue;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -71,7 +72,7 @@ public abstract class BannerInstructions extends DirectionsJsonObject {
   public abstract BannerText sub();
 
   /**
-   * Visual representation of complex upcoming maneuver
+   * Visual representation of complex upcoming maneuver.
    *
    * @return {@link BannerView} representing the secondary visual information
    * @since 3.0.0
--- services-directions/src/test/java/com/mapbox/api/directions/v5/models/JunctionViewResponseTest.java
+++ services-directions/src/test/java/com/mapbox/api/directions/v5/models/JunctionViewResponseTest.java
@@ -5,18 +5,11 @@ import com.mapbox.core.TestUtils;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.Scanner;
-
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JunctionViewResponseTest extends TestUtils {
 
   private static final String BANNER_INSTRUCTION_V5_JUNCTION_VIEW_FIXTURE = "banner_instruction_v5_junction_view.json";
-
-  File f = new File("src/banner_instruction_v5_junction_view.json.txt");
 
   @Test
   public void shouldReadBannerInstruction() throws Exception {
@@ -37,13 +30,11 @@ public class JunctionViewResponseTest extends TestUtils {
     Assert.assertEquals(responseFromJson2, responseFromJson1);
   }
 
-//  @Test
-//  public void testValuesFromJson() throws Exception {
-//    String json = loadJsonFixture(BANNER_INSTRUCTION_V5_JUNCTION_VIEW_FIXTURE);
-//    BannerInstructions responseFromJson = BannerInstructions.fromJson(json);
-//
-////    responseFromJson.routes()
-//    Assert.assertEquals(responseFromJson.view().text(), "CA01610_1_E");
-//
-//  }
+  @Test
+  public void testValuesFromJson() throws Exception {
+    String json = loadJsonFixture(BANNER_INSTRUCTION_V5_JUNCTION_VIEW_FIXTURE);
+    BannerInstructions responseFromJson = BannerInstructions.fromJson(json);
+
+    Assert.assertEquals(responseFromJson.view().text(), "CA01610_1_E");
+  }
 }
