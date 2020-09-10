8683520e6914269c94fc22e78686b09e9ba2cd30
Korshak Nikita <mikita.korshak@mapbox.com>
2020-09-10 14:02:56 +0300

    add tunnelName to StepIntersection (#1179)

--- services-directions-models/src/test/java/com/mapbox/api/directions/v5/models/StepIntersectionTest.java
+++ services-directions-models/src/test/java/com/mapbox/api/directions/v5/models/StepIntersectionTest.java
@@ -38,12 +38,12 @@ public class StepIntersectionTest extends TestUtils {
 
   @Test
   public void testToFromJson1() {
-    StepIntersection intersection
-      = StepIntersection.builder()
+    StepIntersection intersection = StepIntersection.builder()
       .out(0)
       .entry(Arrays.asList(true))
       .bearings(Arrays.asList(125))
       .rawLocation(new double[]{13.426579, 52.508068})
+      .tunnelName("test tunnel")
       .build();
 
     String jsonString = intersection.toJson();
@@ -54,13 +54,13 @@ public class StepIntersectionTest extends TestUtils {
 
   @Test
   public void testToFromJson2() {
-    StepIntersection intersection
-      = StepIntersection.builder()
+    StepIntersection intersection = StepIntersection.builder()
       .in(0)
       .out(1)
       .entry(Arrays.asList(false, true, true))
       .bearings(Arrays.asList(120, 210, 300))
       .rawLocation(new double[]{13.424671, 52.508812})
+      .tunnelName("test tunnel")
       .build();
 
     String jsonString = intersection.toJson();
@@ -73,8 +73,11 @@ public class StepIntersectionTest extends TestUtils {
   @Test
   public void testFromJson() {
     String stepIntersectionJsonString = "{\"out\": 0, \"entry\": [true], \"bearings\": [ 125 ], "
+    + "\"tunnel_name\": \"test tunnel name\","
     + "\"location\": [ 13.426579, 52.508068 ] }";
+
     StepIntersection stepIntersection = StepIntersection.fromJson(stepIntersectionJsonString);
+    Assert.assertEquals("test tunnel name", stepIntersection.tunnelName());
 
     Point location = stepIntersection.location();
     Assert.assertEquals(13.426579, location.longitude(), 0.0001);
