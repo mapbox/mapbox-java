9a30b564ed34f23f59417a35593d98de2f01cf82
Nikita Korshak <mikita.korshak@mapbox.com>
2020-09-10 11:15:37 +0300

    add tunnelName to StepIntersection

--- services-directions-models/src/main/java/com/mapbox/api/directions/v5/models/StepIntersection.java
+++ services-directions-models/src/main/java/com/mapbox/api/directions/v5/models/StepIntersection.java
@@ -127,6 +127,15 @@ public abstract class StepIntersection extends DirectionsJsonObject {
   public abstract List<IntersectionLanes> lanes();
 
   /**
+   * Name of the tunnel. Value may be present if {@link #classes} contains "tunnel".
+   *
+   * @return name of the tunnel
+   */
+  @Nullable
+  @SerializedName("tunnel_name")
+  public abstract String tunnelName();
+
+  /**
    * Convert the current {@link StepIntersection} to its builder holding the currently assigned
    * values. This allows you to modify a single property and then rebuild the object resulting in
    * an updated and modified {@link StepIntersection}.
@@ -244,6 +253,14 @@ public abstract class StepIntersection extends DirectionsJsonObject {
      * @since 3.0.0
      */
     public abstract Builder lanes(@Nullable List<IntersectionLanes> lanes);
+
+    /**
+     * Name of the tunnel. Value is present only if class is tunnel.
+     *
+     * @param tunnelName name of the tunnel
+     * @return this builder for chaining options together
+     */
+    public abstract Builder tunnelName(@Nullable String tunnelName);
 
     /**
      * The rawLocation as a double array. Once the {@link StepIntersection} object's created,
